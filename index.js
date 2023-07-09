b509e53c77e6bffde5c1bc0d2da87b48596a4301
Debanjan2000 <91735642+Debanjan2000@users.noreply.github.com>
2023-07-09 21:49:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var randomNumber1=Math.floor(Math.random()*6)+1;//gettting tandom no from 1-6
-var new_img="images\\Dice"+randomNumber1+".png";//using concatenation to generate any dice img from Dice1.png->to Dice2.png
+var new_img="Dice"+randomNumber1+".png";//using concatenation to generate any dice img from Dice1.png->to Dice2.png
 document.querySelectorAll("img")[0].setAttribute("src",new_img);//now querySelectorALL returns a LIST,so tapping
 //into the first element and modifying the contents of the src attribute.
 
 var randomNumber2=Math.floor(Math.random()*6)+1;
-var new_img="images\\Dice"+randomNumber2+".png";
+var new_img="Dice"+randomNumber2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",new_img);
 
 //comparing and giving the output by changing the h1 element content.
